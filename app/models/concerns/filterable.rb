ce1956b237d845b6a23cc25cf0ea42ae1ab9eef5
Rodette Pedro <rpedro@jway.com>
2018-07-29 17:22:58 +0800

    Fix subfiltering.

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -13,18 +13,19 @@ module Filterable
     # Wherein, table is optional and defaults to self.table_name
     def filter(params)
       query = self.where(nil)
+      
       params.each do |first|
         first[1].each do |second|
           if second[1].is_a?(Hash)
             second[1].each do |operator, value| 
               next if value.nil?
               filter = Filter.new(first[0], second[0], operator, value)
-              query.where(filter.build)
+              query = query.where(filter.build)
             end
           else
             next if second[1].nil?
             filter = Filter.new(self.table_name, first[0], second[0], second[1])
-            query.where(filter.build)
+            query = query.where(filter.build)
           end
         end
       end
